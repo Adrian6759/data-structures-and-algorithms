89ae0f475bf1f3d886742ed021c0d3bc8fe62002
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-01-13 20:00:48 -0800

    Got through first function

---
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
@@ -0,0 +1,17 @@
+package datastructures.linkedlist;
+
+public class Node
+{
+  public int value;
+  public Node next = null;
+
+  Node(int value)
+  {
+    this.value = value;
+  }
+//  Node(int value, Node next) {
+//    this.value = value;
+//    this.next = next;
+//  }
+
+}
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -9,7 +9,11 @@ public class LinkedListTest
   @Test void testAMethod()
     {
       LinkedList sut = new LinkedList();
-      System.out.println(sut);
+      //Head = null because LinkedList makes head null
+      sut.insert(3);
+      // Head.value = 3, next = null First time around
+      sut.insert(1000);
       assertTrue(true);
     }
+
 }
