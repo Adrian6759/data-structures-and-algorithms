b24bae00ef8140954a458f33054a11bcad73dc7d
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-01-30 01:45:33 -0800

    Added functions and tests

--- java/datastructures/lib/src/test/java/codechallenges/linkedlist/LinkedListChallengesTest.java
+++ java/datastructures/lib/src/test/java/codechallenges/linkedlist/LinkedListChallengesTest.java
@@ -14,4 +14,31 @@ public class LinkedListChallengesTest
     System.out.println("test something here");
     assertTrue(true);
   }
+  @Test void testAppend(){
+    LinkedList sut = new LinkedList();
+    sut.insert(1);
+    sut.insert(2);
+    sut.insert(3);
+    sut.append(5);
+    assertEquals("{3} -> {2} -> {1} -> {5} -> NULL", sut.toString())
+
+  }
+  @Test void testInsertBefore(){
+    LinkedList sut = new LinkedList();
+    sut.insert(1);
+    sut.insert(2);
+    sut.insert(4);
+    sut.insertBefore(4, 3);
+    assertEquals("{3} -> {4} -> {2} -> {1} -> NULL", sut.toString())
+
+  }
+  @Test void testInsertAfter(){
+    LinkedList sut = new LinkedList();
+    sut.insert(1);
+    sut.insert(2);
+    sut.insert(4);
+    sut.insertAfter(4, 3);
+    assertEquals("{4} -> {3} -> {2} -> {1} -> NULL", sut.toString())
+
+  }
 }
